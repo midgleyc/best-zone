cc339eb6666f89d7b2183b93da99ab5f2fda776f
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-03-01 09:27:50 +0000

    feat: add autumnaton_value script

---
+++ README.md
@@ -0,0 +1,5 @@
+Show the best zone to send your autumn-aton to, along with the profit you could make.
+
+Assumes max acuity for simplicity.
+
+Uses the `averageAutumnatonValue` function from [`garbo`](https://github.com/Loathing-Associates-Scripting-Society/garbage-collector), plus all dependencies.
---
+++ scripts/autumnaton_value.js
@@ -0,0 +1,121 @@
+const k = require('kolmafia')
+Object.assign(globalThis, k);
+
+if (!Array.prototype.flat) {
+	Object.defineProperty(Array.prototype, 'flat', {
+		configurable: true,
+		value: function flat () {
+			var depth = isNaN(arguments[0]) ? 1 : Number(arguments[0]);
+
+			return depth ? Array.prototype.reduce.call(this, function (acc, cur) {
+				if (Array.isArray(cur)) {
+					acc.push.apply(acc, flat.call(cur, depth - 1));
+				} else {
+					acc.push(cur);
+				}
+
+				return acc;
+			}, []) : Array.prototype.slice.call(this);
+		},
+		writable: true
+	});
+}
+
+const garboRegularValueCache = new Map();
+const garboHistoricalValueCache = new Map();
+function garboValue(item, useHistorical) {
+  const cachedValue =
+    garboRegularValueCache.get(item) == null ? 
+    (useHistorical ? garboHistoricalValueCache.get(item) : undefined) :garboRegularValueCache.get(item);
+  if (cachedValue === undefined) {
+    const value = garboSaleValue(item, useHistorical);
+    (useHistorical ? garboHistoricalValueCache : garboRegularValueCache).set(item, value);
+    return value;
+  }
+  return cachedValue;
+}
+
+function garboSaleValue(item, useHistorical) {
+  if (useHistorical) {
+    if (historicalAge(item) <= 7.0 && historicalPrice(item) > 0) {
+      const isMallMin = historicalPrice(item) === Math.max(100, 2 * autosellPrice(item));
+      return isMallMin ? autosellPrice(item) : 0.9 * historicalPrice(item);
+    }
+  }
+  return getSaleValue(item);
+}
+
+const valueMap = new Map();
+const MALL_VALUE_MODIFIER = 0.9;
+/**
+ * Returns the average value--based on mallprice and autosell--of a collection of items
+ * @param items items whose value you care about
+ */
+function getSaleValue(item) {
+        if (valueMap.has(item))
+            return valueMap.get(item) || 0;
+        if (item.discardable) {
+            valueMap.set(item, mallPrice(item) > Math.max(2 * autosellPrice(item), 100)
+                ? MALL_VALUE_MODIFIER * mallPrice(item)
+                : autosellPrice(item));
+        }
+        else {
+            valueMap.set(item, mallPrice(item) > 100 ? MALL_VALUE_MODIFIER * mallPrice(item) : 0);
+        }
+        return valueMap.get(item) || 0;
+}
+
+function sum(addends, mappingFunction) {
+    return addends.reduce((subtotal, element) => subtotal + mappingFunction(element), 0);
+}
+
+function averageAutumnatonValue(
+  location
+) {
+  const badAttributes = ["LUCKY", "ULTRARARE", "BOSS"];
+  const rates = appearanceRates(location);
+  const monsters = Object.keys(getLocationMonsters(location))
+    .map((m) => toMonster(m))
+    .filter((m) => !badAttributes.some((s) => m.attributes.includes(s)) && rates[m.name] > 0);
+
+  if (monsters.length === 0) {
+    return 0;
+  } else {
+    const maximumDrops = 5;
+    const acuityCutoff = 20 - (3) * 5;
+    const validDrops = monsters
+      .map((m) => itemDropsArray(m))
+      .flat()
+      .map(({ rate, type, drop }) => ({
+        value: !["c", "0"].includes(type) ? garboValue(drop, true) : 0,
+        preAcuityExpectation: ["c", "0", ""].includes(type) ? (2 * rate) / 100 : 0,
+        postAcuityExpectation:
+          rate >= acuityCutoff && ["c", "0", ""].includes(type) ? (8 * rate) / 100 : 0,
+      }));
+    const overallExpectedDropQuantity = sum(
+      validDrops,
+      ({ preAcuityExpectation, postAcuityExpectation }) =>
+        preAcuityExpectation + postAcuityExpectation
+    );
+    const expectedCollectionValue = sum(
+      validDrops,
+      ({ value, preAcuityExpectation, postAcuityExpectation }) => {
+        // This gives us the adjusted amount to fit within our total amount of available drop slots
+        const adjustedDropAmount =
+          (preAcuityExpectation + postAcuityExpectation) *
+          Math.min(1, maximumDrops / overallExpectedDropQuantity);
+        return adjustedDropAmount * value;
+      }
+    );
+    return expectedCollectionValue;
+  }
+}
+
+function main() {
+  const autumnMap = new Map(Location.all().filter(x => x.parent != "Removed").map(l => [l, averageAutumnatonValue(l)]).sort((a, b) => a[1] - b[1]));
+  for (let v of autumnMap) {
+    print(`${v[1].toFixed(0)} - ${v[0]} - ${v[0].zone}`)
+  }
+}
+
+module.exports.main = main
